bb8118ab511a02a36dd6692e336e9d54f44b7154
LennyBla <thais.florentin26@gmail.com>
2024-05-29 16:38:17 -0300

    Axios

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,25 +1,20 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
-import autenticaStore from '../stores/autentica.store';
 
 const httpV1 = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/'
+    baseURL: 'https://programacaododia-back.vercel.app/api/v1/'
+    ///baseURL: 'http://localhost:8000/api/v1/'
 });
 
 const httpV2 = axios.create({
-    baseURL: 'http://localhost:8000/api/v2/',
+    //baseURL: 'http://localhost:8000/api/v2/',
+    baseURL: 'https://programacaododia-back.vercel.app/api/v2/',
     withCredentials: true
 });
 
 httpV2.interceptors.request.use((config) => {
     if (!config.headers) {
         config.headers = {};
-    }
-
-    // Adiciona o token JWT ao cabeçalho de autorização
-    const token = autenticaStore.usuario.token;
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
     }
 
     try {
