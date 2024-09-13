6a6c5f96d4f5a4766f8c64985fb0f93b7acac5af
LennyBla <thais.florentin26@gmail.com>
2024-09-13 16:15:53 -0300

    arruando aslguns componets

--- src/paginas/Administracao/Logs/LoginAdm/Login.tsx
+++ src/paginas/Administracao/Logs/LoginAdm/Login.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { toast, ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Botao from '../../../../componentes/Botoes/Botao/Button';
-import CampoDigitacao from '../../Campo/Digite';
+import { TextField, Typography, Container, Box, Paper, Button as MuiButton } from '@mui/material';
 import usePost from '../../../../hook/usePost';
 import autenticaStore from '../../../../stores/autentica.store';
 import styles from './Login.module.scss';
 import LogoCataratas from '../../../../asset/cataratasparkhotel.png';
+import Botao from '../../../../componentes/Botoes/Botao/Button';  // O componente personalizado
 
 function Login() {
     const [username, setUsername] = useState<string>('');
@@ -61,38 +61,65 @@ function Login() {
                 transition={Slide} 
                 style={{ top: '10px' }}
             />
-            <button onClick={handleBack} className={styles.backButton}>
-                <FontAwesomeIcon icon={faArrowLeft} /> Voltar
-            </button>
+            <MuiButton onClick={handleBack} startIcon={<FontAwesomeIcon icon={faArrowLeft} />} sx={{ mb: 2, mt: 2 }}>
+                Voltar
+            </MuiButton>
 
-            <div className={styles.LoginContainer}>
-                <form onSubmit={handleSubmit}>
-                    <div className={styles.logoContainer}>
-                        <img src={LogoCataratas} alt="Cataratas Park Hotel Logo" />
-                    </div>
+            <Container maxWidth="xs" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <Paper 
+                    elevation={6} 
+                    sx={{ 
+                        p: 4, 
+                        borderRadius: 5, 
+                        width: '100%', 
+                        maxWidth: 400,
+                        textAlign: 'center',
+                    }}
+                >
+                    <Box display="flex" flexDirection="column" alignItems="center" mb={2}>
+                        <img src={LogoCataratas} alt="Cataratas Park Hotel Logo" style={{ width: '400px', marginBottom: '20px' }} />
+                        <Typography component="h1" variant="h5">
+                            Login
+                        </Typography>
+                    </Box>
 
-                    <h1 className={styles.Titulo}>Login</h1>
-                    <div>
-                        <CampoDigitacao
-                            valor={username}
-                            onChange={setUsername}
-                            placeholder='Insira seu E-mail'
-                            label='E-mail'
-                            tipo='text'
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="E-mail"
+                            type="email"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                         />
-                        <CampoDigitacao
-                            valor={password}
-                            onChange={setPassword}
-                            placeholder='Digite a senha'
-                            label='Senha'
-                            tipo='password'
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Senha"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
-                    </div>
-                    <div>
-                        <Botao classe={styles.botao} tipo="submit">Entrar</Botao>
-                    </div>
-                </form>
-            </div>
+                        
+                        <Botao classe={styles.botao} tipo="submit">
+                            Entrar
+                        </Botao>
+                    </form>
+
+                    <MuiButton
+                        component={Link}
+                        to="/cadastroAdm"
+                        fullWidth
+                        variant="text"
+                        color="primary"
+                        sx={{ mt: 3 }}
+                    >
+                      Cadastre-se
+                    </MuiButton>
+                </Paper>
+            </Container>
         </>
     );
 }
